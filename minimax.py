3e88bfbf772238e327bca6f3f2c494bb5366f665
JMC <jeffc2000@outlook.com>
2022-10-27 15:05:29 -0400

    Progress

--- minimax.py
+++ minimax.py
@@ -8,7 +8,7 @@ class MinimaxPlayer(Konane, Player):
         self.side = side
         self.name = "MinimaxPlayer"
     def getMove(self, board):
-        
+        self.getMoveHelper(board, 0)
         moves = self.generateMoves(board, self.side)
         # nextboard??
         n = len(moves)
@@ -18,12 +18,27 @@ class MinimaxPlayer(Konane, Player):
             for i in moves:
                 return
     def getMoveHelper(self, board, depth):
-        return
+        if (depth % 2 == 0):
+            currentTurn = self.side
+            maximizing = True
+        else:
+            currentTurn = self.opponent
+            maximizing = False
+        # figure out if this is a min or max turn and figure out the optimal move in generateMoves
+        moveList = []
+        
+        if depth == self.depthLimit:
+            return eval(board)
+        for move in self.generateMoves(board, currentTurn):
+            tempTuple = (move, self.getMoveHelper(self.nextBoard(board, currentTurn, move), depth + 1))
+            moveList.append(tempTuple)
+        if maximizing:
+             max(moveList[0[1]])
+        else:
+            min(moveList[0[1]])
     
     def eval(self, board):
         self.countSymbol(board, self.side)
-        # easy eval function: counting number of your spaces on the board. you want to keep your pieces,
-        # much like checkers and chess.
         #complete this – this will be your evaluation function. High should be good for max.
         
 game = Konane(8)
