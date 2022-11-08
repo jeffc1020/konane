07bd3ff2c4c57e8c71e1029da693cc9775d25f7a
JMC <jeffc2000@outlook.com>
2022-11-08 15:05:06 -0500

    More progress (kind of)

--- minimax.py
+++ minimax.py
@@ -1,3 +1,4 @@
+from json.encoder import INFINITY
 from konane import *
 
 class MinimaxPlayer(Konane, Player):
@@ -8,7 +9,8 @@ class MinimaxPlayer(Konane, Player):
         self.side = side
         self.name = "MinimaxPlayer"
     def getMove(self, board): # finds the optimal next move
-        return self.getMoveHelper(board, 0)
+        #return self.getMoveHelper(board, 0)
+        return self.getMoveHelper(board, 0, self.side)
         # moves = self.generateMoves(board, self.side)
         # # nextboard??
         # n = len(moves)
@@ -17,7 +19,25 @@ class MinimaxPlayer(Konane, Player):
         # else:
         #     for i in moves:
         #         return
-    def getMoveHelper(self, board, depth):
+    def getMoveHelper(self, board, depth, currentTurn):
+        if (depth == self.limit):
+            return eval(board)
+        if (currentTurn == self.side): # maximizing
+            value = -1000000
+            for child in self.generateMoves(board, currentTurn):
+                value = max(self.getMoveHelper(
+                    self.nextBoard(board, currentTurn, child), depth + 1, self.opponent(self.side)))
+            return value
+        else: # minimizing
+            value = 1000000
+            for child in self.generateMoves(board, currentTurn):
+                value = min(self.getMoveHelper(
+                    self.nextBoard(board, currentTurn, child), depth + 1, self.side))
+            return value
+        
+        """
+        if depth == self.limit: # base case: we are at a leaf node
+            return eval(board)# return the heuristic (eval function) of this leaf node
         if (depth % 2 == 0): # if we are at an even depth 
             currentTurn = self.side # it's our turn (rather than the opponent's)
             maximizing = True
@@ -27,20 +47,24 @@ class MinimaxPlayer(Konane, Player):
             maximizing = False
         # figure out if this is a min or max turn and figure out the optimal move in generateMoves
         moveList = []
-        if depth == self.depthLimit: # base case: we are at a leaf node
-            return eval(board) # return the heuristic (eval function) of this leaf node
-        for move in self.generateMoves(board, currentTurn): # loops through each next move for the current node
-            tempTuple = (move, self.getMoveHelper(self.nextBoard(board, currentTurn, move), depth + 1))
+         # return the heuristic (eval function) of this leaf node
+        for mov in self.generateMoves(board, currentTurn): # loops through each next move for the current node
+            nextBoard = self.nextBoard(board, currentTurn, mov)
+            nextMove = self.getMoveHelper(nextBoard, depth + 1)
+            tempTuple = (mov, nextMove)
             moveList.append(tempTuple)
         # actually choose the next move
         if maximizing:
             # choose the move with the max associated value
-            max(moveList[0[1]]) # needs fixin
+            return max(value[1] for value in moveList)
+            #return max(moveList[0[1]]) # needs fixin
         else:
             # choose the move with the min associated value
-            min(moveList[0[1]]) # needs fixin
-    
+            return min(value[1] for value in moveList)
+            #return min(moveList[0[1]]) # needs fixin
+        """
     def eval(self, board):
-        self.countSymbol(board, self.side) # should be good for now
+        return self.countSymbol(board, self.side) # should be good for now
+        
 game = Konane(8)
-game.playNGames(1, SimplePlayer(8), RandomPlayer(8), 1)
+game.playNGames(2, MinimaxPlayer(8, 4), MinimaxPlayer(8, 4), 2)
