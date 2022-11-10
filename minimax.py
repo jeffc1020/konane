43e5a6a67535f60f31480c95a597cae85fbb1180
JMC <jeffc2000@outlook.com>
2022-11-10 17:47:55 -0500

    Nvm it doesn't actually work

--- minimax.py
+++ minimax.py
@@ -10,11 +10,11 @@ class MinimaxPlayer(Konane, Player):
         self.name = "MinimaxPlayer"
     def getMove(self, board): # finds the optimal next move
         #return self.getMoveHelper(board, 0)
-        bestMoveValue = self.getMoveHelper(board, 0, self.side)
         availableMoves = self.generateMoves(board, self.side)
         if (len(availableMoves) == 0):
             return []
         else:
+            bestMoveValue = self.getMoveHelper(board, 0, self.side)
             for child in availableMoves:
                 if (self.eval(self.nextBoard(board, self.side, child)) == bestMoveValue):
                     return child
@@ -39,36 +39,8 @@ class MinimaxPlayer(Konane, Player):
                 value = min(value, nextIteration)
             return value
         
-        """
-        if depth == self.limit: # base case: we are at a leaf node
-            return eval(board)# return the heuristic (eval function) of this leaf node
-        if (depth % 2 == 0): # if we are at an even depth 
-            currentTurn = self.side # it's our turn (rather than the opponent's)
-            maximizing = True
-            # I may have this backwards
-        else: # else, we are at an odd depth
-            currentTurn = self.opponent # it's the opponent's turn
-            maximizing = False
-        # figure out if this is a min or max turn and figure out the optimal move in generateMoves
-        moveList = []
-         # return the heuristic (eval function) of this leaf node
-        for mov in self.generateMoves(board, currentTurn): # loops through each next move for the current node
-            nextBoard = self.nextBoard(board, currentTurn, mov)
-            nextMove = self.getMoveHelper(nextBoard, depth + 1)
-            tempTuple = (mov, nextMove)
-            moveList.append(tempTuple)
-        # actually choose the next move
-        if maximizing:
-            # choose the move with the max associated value
-            return max(value[1] for value in moveList)
-            #return max(moveList[0[1]]) # needs fixin
-        else:
-            # choose the move with the min associated value
-            return min(value[1] for value in moveList)
-            #return min(moveList[0[1]]) # needs fixin
-        """
     def eval(self, board):
         return self.countSymbol(board, self.side) # should be good for now
         
 game = Konane(8)
-game.playNGames(1, MinimaxPlayer(8, 1), MinimaxPlayer(8, 1), 1)
+game.playNGames(1, MinimaxPlayer(8, 2), MinimaxPlayer(8, 2), 1)
